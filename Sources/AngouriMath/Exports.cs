9450c384b1e45e7230593ddc588bf246fdebb2a9
Robin Sue <robinsue@live.de>
2021-03-12 01:00:59 +0100

    Compile NativeAOT Library

---
+++ Sources/AngouriMath/Exports.cs
@@ -0,0 +1,22 @@
+﻿/* 
+ * Copyright (c) 2019-2021 Angouri.
+ * AngouriMath is licensed under MIT. 
+ * Details: https://github.com/asc-community/AngouriMath/blob/master/LICENSE.md.
+ * Website: https://am.angouri.org.
+ */
+using AngouriMath.Core.Exceptions;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using static AngouriMath.Entity;
+
+namespace AngouriMath
+{
+    public static class Exports
+    {
+        [UnmanagedCallersOnly(EntryPoint = "add")]
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
